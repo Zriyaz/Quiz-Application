2f95f2b435160de4ecfed1c6ca08bf6d6ff822d3
Md Riyaz Kazmi <mdriyazuddine17@gmail.com>
2020-06-13 09:26:29 +0000

    //commmit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,28 @@
-import React from "react"
-import {Link} from "react-router-dom"
-import {Helmet} from "react-helmet" 
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
 
+const Home = () => (
+    <Fragment>
+        <Helmet><title>Home - Quiz App</title></Helmet>
+        <div id="home">
+            <section>
+                <div style={{ textAlign: 'center' }}>
+                    <span className="mdi mdi-cube-outline cube"></span>
+                </div>
+                <h1>Quiz App</h1>
+                <div className="play-button-container">
+                    <ul>
+                        <li><Link className="play-button" to="/play/instructions">Play</Link></li>
+                    </ul>
+                </div>
+                <div className="auth-container">
+                    <Link to="/login" className="auth-buttons" id="login-button">Login</Link>
+                    <Link to="/register" className="auth-buttons" id="signup-button">Sign up</Link>
+                </div>
+            </section>
+        </div>
+    </Fragment>
+);
 
-const Home = () =>(
-  <React.Fragment>
-  <Helmet><title>Quiz App Home</title></Helmet>
-    <div id="home">
-      <section>
-        <div style={{textAlign:'center'}}>
-          <span className="mdi mdi-cube-outline cube"></span>
-        </div>
-        <h1>Quiz App</h1>
-        <div className="play-button-container">
-          <ul>
-            <li> <Link className="play-button" to ="/play/instructions">play</Link></li>
-          </ul>
-        </div>
-        <div className="auth-container">
-        <Link className="auth-buttons" id="login-button" to ="/login">login here</Link>
-        <Link className="auth-buttons"  id="signup-button" to ="/register">Sign Up</Link>
-        </div>
-      </section>
-    </div>
-  </React.Fragment>
-)
-export default Home
+export default Home;
